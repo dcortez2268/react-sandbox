2a780b5744194c40497cb0efc823207b052e53dd
Dominick Cortez <jdcortez2268@gmail.com>
2022-09-06 12:38:09 -0700

    start sandbox project

---
+++ r-notes-3.js
@@ -0,0 +1,5 @@
+/*************************************************************************************************
+Course Title: React Front to back 2022
+Author: Traversy Media
+Description: Project 3, React Sandbox, small project to examine useRef, useCallback, useMemo, custom hooks
+*************************************************************************************************/
